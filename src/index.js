2d2b066b20e7d6f7c628903da27ba753fd3d5a45
Pegoraro, Luisa <luisapegoraro@quickenloans.com>
2021-04-16 15:51:32 -0400

    Added store, actions, reducer and dispatcher

--- src/index.js
+++ src/index.js
@@ -3,9 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
 
+
+// ACTION describes what actions can be done
+const increment = () => {
+    return {
+        type: 'INCREMENT'
+    }
+}
+
+const decrement = () => {
+    return {
+        type: 'DECREMENT'
+    }
+}
+
+// REDUCER is where the state goes from the initial to the next. 
+// Based on the value of the ACTION that is passed to it, it will modify the state accordingly. 
+const counter = (state = 0, action) => {
+    switch(action.type){
+        case "INCREMENT":
+            return state + 1;
+        case "DECREMENT":
+            return state - 1;
+    }
+}
 
 // STORE is a globalized state
+let store = createStore(counter);
+
+store.subscribe(() => {
+    console.log("[STORE SUBSCRIBE] Current state: ", store.getState());
+})
+
+// DISPATCH where the action is actually sent to the REDUCER which in turn updates the state. 
+store.dispatch(increment());
 
 ReactDOM.render(
   <React.StrictMode>
