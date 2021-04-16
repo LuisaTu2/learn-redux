a17ab4ed1dc9d9da69bdb38a0810b0d3c56b5a3d
Pegoraro, Luisa <luisapegoraro@quickenloans.com>
2021-04-16 15:56:39 -0400

    This was w/o react. Now onto adding react!

--- src/index.js
+++ src/index.js
@@ -6,39 +6,45 @@ import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 
 
-// ACTION describes what actions can be done
-const increment = () => {
-    return {
-        type: 'INCREMENT'
-    }
-}
 
-const decrement = () => {
-    return {
-        type: 'DECREMENT'
-    }
-}
 
-// REDUCER is where the state goes from the initial to the next. 
-// Based on the value of the ACTION that is passed to it, it will modify the state accordingly. 
-const counter = (state = 0, action) => {
-    switch(action.type){
-        case "INCREMENT":
-            return state + 1;
-        case "DECREMENT":
-            return state - 1;
-    }
-}
 
-// STORE is a globalized state
-let store = createStore(counter);
+// THIS IS ONE WAY BUT WITHOUT INCLUDING REACT
+// // ACTION describes what actions can be done
+// const increment = () => {
+//     return {
+//         type: 'INCREMENT'
+//     }
+// }
 
-store.subscribe(() => {
-    console.log("[STORE SUBSCRIBE] Current state: ", store.getState());
-})
+// const decrement = () => {
+//     return {
+//         type: 'DECREMENT'
+//     }
+// }
 
-// DISPATCH where the action is actually sent to the REDUCER which in turn updates the state. 
-store.dispatch(increment());
+// // REDUCER is where the state goes from the initial to the next. 
+// // Based on the value of the ACTION that is passed to it, it will modify the state accordingly. 
+// const counter = (state = 0, action) => {
+//     switch(action.type){
+//         case "INCREMENT":
+//             return state + 1;
+//         case "DECREMENT":
+//             return state - 1;
+//     }
+// }
+
+// // STORE is a globalized state
+// let store = createStore(counter);
+
+// store.subscribe(() => {
+//     console.log("[STORE SUBSCRIBE] Current state: ", store.getState());
+// })
+
+// // DISPATCH where the action is actually sent to the REDUCER which in turn updates the state. 
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
 
 ReactDOM.render(
   <React.StrictMode>
