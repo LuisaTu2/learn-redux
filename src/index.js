163f1768ae6c036c518bf660f183fb57dc5a67ab
Pegoraro, Luisa <luisapegoraro@quickenloans.com>
2021-04-16 15:18:49 -0400

    Setting up

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+// STORE is a globalized state
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
